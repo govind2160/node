5f8f5938f8f70b2869a7def90c1f7b9132a552ee
govind <govind22154057@akgec.ac.in>
2025-06-28 15:42:55 +0530

    new

---
+++ index.js
@@ -0,0 +1,31 @@
+const lib = require('./lib.js')
+const express = require('express')
+ console.log("hello");
+
+const server= express();
+server.listen(8080);
+
+
+
+
+
+
+
+
+
+// import {sum, diff} from './lib.js';
+// const fs = require('fs');
+
+// // const txt = fs.readFileSync('demo.txt','utf-8')
+
+// fs.readFile('demo.txt','utf-8',(err ,txt)=>{
+//     console.log(txt)
+// });
+
+// // console.log(txt);
+// console.log(lib.sum(3,4),lib.diff(3,6));
+
+// const a=4;
+
+
+

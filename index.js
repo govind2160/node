3e95a6a4c09944a547204a5968c3460316b167e7
govind <govind22154057@akgec.ac.in>
2025-06-30 14:13:10 +0530

    update

--- index.js
+++ index.js
@@ -1,45 +1,76 @@
-const http = require('http');
 const fs = require('fs');
 const index = fs.readFileSync('index.html','utf-8');
 const data = JSON.parse(fs.readFileSync('data.json','utf-8'));
 
 const products= data.products;
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    if(req.url.startsWith('/product')){
-        const id = req.url.split('/')[2]
-        const product= products.find(p=>p.id===(+id))  
-        console.log(product);
-          res.setHeader('Content-Type','text/html'); 
-            let modifiedIndex=index.replace('**title**',product.title)
-            .replace('**url**',product.thumbnail)
-            .replace('**price**',product.price)
-            .replace('**rating**',product.rating);
-            res.end(modifiedIndex);
-            return;
-    }
-    
 
 
 
-    switch(req.url){
-        case '/':
-            res.setHeader('Content-Type','text/html'); 
-            res.end(index);
-            break;
-        case '/api':
-            res.setHeader('Content-Type','application/json'); 
-            res.end(JSON.stringify(data));
-            break;
-       
-        default:
-            res.writeHead(404,'NOT FOUND');
-            res.end()
-            
-    }
-     
-   
+const express = require ('express');
+
+const server = express(); 
+
+server.use(express.json());
+server.use((req,res,next)=>{
+    //console.log(req.method,req.ip,req.hostname,new Date(),req.get('User-Agent'))
+    next()
 })
 
-server.listen(8080)   
+const auth = ((req,res,next)=>{
+    //console.log(req)
+    //console.log(req.query)
+    if(req.body.password=='123'){
+         next()
+    }
+    else{ 
+        res.sendStatus(401);
+
+    }
+   
+})
+//server.use(auth);
+
+ 
+//API - EndPoint-Route
+server.get('/',auth,(req,res)=>{
+    res.json({type:'GET'})
+})
+server.post('/',auth,(req,res)=>{
+    res.json({type:'POST'})
+})
+server.put('/',auth,(req,res)=>{
+    res.json({type:'PUT'})
+})
+server.delete('/',auth,(req,res)=>{
+    res.json({type:'DELETE'})
+})
+server.patch('/',auth,(req,res)=>{
+    res.json({type:'PATCH'})
+})
+
+
+
+
+
+
+
+
+server.get('/demo',(req,res)=>{
+    //res.json(products)
+    //res.sendStatus(404); 
+
+   // res.send('<h1>hello</h1>')
+//    res.sendFile('C:\Users\govin\Desktop\node\index.html');
+
+})
+
+
+
+
+
+ 
+ server.listen(8080,()=>{
+    console.log('server started')
+ } )
+  

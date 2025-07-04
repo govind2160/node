03a13e852a8d4552ade23322ed1d43eec975d6f3
govind <govind22154057@akgec.ac.in>
2025-07-04 22:20:32 +0530

    update 1

--- index.js
+++ index.js
@@ -1,31 +1,108 @@
-const lib = require('./lib.js')
-const express = require('express')
- console.log("hello");
+const fs = require('fs');
+const index = fs.readFileSync('index.html','utf-8');
+const data = JSON.parse(fs.readFileSync('data.json','utf-8'));
 
-const server= express();
-server.listen(8080);
-
+const products= data.products;
 
 
 
 
+const express = require ('express');
+const morgan= require('morgan');
+
+const server = express(); 
+
+server.use(express.json());
+server.use(morgan('default'));
+server.use(express.static('public'));
 
 
 
 
-// import {sum, diff} from './lib.js';
-// const fs = require('fs');
-
-// // const txt = fs.readFileSync('demo.txt','utf-8')
-
-// fs.readFile('demo.txt','utf-8',(err ,txt)=>{
-//     console.log(txt)
-// });
-
-// // console.log(txt);
-// console.log(lib.sum(3,4),lib.diff(3,6));
-
-// const a=4;
+ 
+//API - EndPoint-Route
 
 
 
+
+
+//Create API  /products
+
+server.post('/products',(req,res)=>{
+    console.log(req.body);
+    products.push(req.body);
+    res.json(req.body) 
+})
+
+
+//Products API READ
+server.get('/products',(req,res)=>{
+    
+    res.json(products);
+})
+server.get('/products/:id',(req,res)=>{
+    const id = +req.params.id;
+    const product = products.find(p => p.id === id);
+
+    res.json(product);
+})
+
+
+//Update API    PUT API
+
+
+server.put('/products/:id',(req,res)=>{
+    const id = +req.params.id;
+    const product = products.find(p => p.id === id);
+    if (!product) {
+        return res.status(404).json({ error: 'Product not found' });
+    }
+    //new product data
+    //hello
+    //hhubi
+    
+
+
+
+    res.json(product);
+})
+
+
+
+
+
+
+
+server.delete('/',(req,res)=>{
+    res.json({type:'DELETE'})
+})
+// server.patch('/',auth,(req,res)=>{
+//     res.json({type:'PATCH'})
+
+// })
+
+
+
+
+
+
+
+
+server.get('/demo',(req,res)=>{
+    //res.json(products)
+    //res.sendStatus(404); 
+
+   // res.send('<h1>hello</h1>')
+//    res.sendFile('C:\Users\govin\Desktop\node\index.html');
+
+})
+
+
+
+
+
+ 
+ server.listen(8080,()=>{
+    console.log('server started')
+ } )
+  

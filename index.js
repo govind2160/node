281b2399d6586d1263ef2469853f3fe90de3f37b
govind <govind22154057@akgec.ac.in>
2025-07-04 22:15:12 +0530

    ch -4 new

--- index.js
+++ index.js
@@ -54,7 +54,11 @@ server.get('/products/:id',(req,res)=>{
 server.put('/products/:id',(req,res)=>{
     const id = +req.params.id;
     const product = products.find(p => p.id === id);
-    
+    if (!product) {
+        return res.status(404).json({ error: 'Product not found' });
+    }
+
+
 
     res.json(product);
 })
